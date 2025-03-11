8decf946c45147616782aa425eae71b541a32da5
Prince Kumar Singh <120776641+PrinceSingh78@users.noreply.github.com>
2025-03-11 07:33:55 +0000

    Datatypes Summary

---
+++ 01_Basic/04_Comparison.js
@@ -0,0 +1,11 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null > undefined);  // Iss tarah k conversion ko avoid kariye.....
+
+
+// === 
+
+console.log("2" === 2)
+
