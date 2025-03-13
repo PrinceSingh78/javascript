d693783ca6962a44d24398e7ae7be4162ab85585
Prince Kumar Singh <120776641+PrinceSingh78@users.noreply.github.com>
2025-03-13 18:47:02 +0000

    dates

--- 01_Basic/07_Dates.js
+++ 01_Basic/07_Dates.js
@@ -1,2 +1,31 @@
 let myDate = new Date()
 console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString);
+console.log(typeof myDate);
+
+// let createdDate = new Date(2027,3,24,6,8)
+let createdDate = new Date(2027,3,24,6,8)
+console.log(createdDate.toLocaleString());
+
+// let myStamp = Date.now()
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
+
+
+
