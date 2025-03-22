2b29b84b0ded5b48502a745a482730e6e57a78de
Prince Kumar Singh <120776641+PrinceSingh78@users.noreply.github.com>
2025-03-22 16:19:35 +0000

    if else

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,21 @@
+// if
+
+if(2 == "2"){
+    console.log("executed");
+    
+
+}
+
+// <,>,<=,>=,==,!=,===
+
+const userId = "Prince@qww"
+if(userId){
+    console.log(`Have a user Id ${userId}`);
+}else{
+    console.log(`Dont Have a user Id ${userId}`);
+    
+}
+
+// falsy value :- false,0,-0,"",null,undefined,NaN
+
+//truthy value :- "0",'false',"",[],{},function(){}
